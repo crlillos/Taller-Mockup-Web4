db66f7e2478341017db11531180e83bd31e2b7fa
jmc0109-prog <jmc0109@alu.medac.es>
2026-04-14 18:37:58 +0200

    Firebase hecho
    Aqui esta hecho el firebase

---
+++ Taller-Mockup-Web4-Juan-Alejandro (1)/Taller-Mockup-Web4-Juan-Alejandro/script.js
@@ -0,0 +1,47 @@
+
+const botonesNav = document.querySelectorAll('.nav-btn');
+const secciones = document.querySelectorAll('.seccion');
+
+botonesNav.forEach(boton => {
+    boton.addEventListener('click', () => {
+        const destino = boton.dataset.target;
+
+     
+        botonesNav.forEach(b => b.classList.remove('active'));
+        secciones.forEach(s => s.classList.remove('activa'));
+
+       
+        boton.classList.add('active');
+        document.getElementById(destino).classList.add('activa');
+
+
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+});
+
+document.getElementById("calcularBtn").addEventListener("click", () => {
+    const servicio = parseFloat(document.getElementById("servicio").value);
+    const horas = parseFloat(document.getElementById("horas").value);
+    const precioHora = parseFloat(document.getElementById("precioHora").value);
+
+    if (servicio === 0) {
+        alert("Selecciona un servicio válido");
+        return;
+    }
+
+    const total = servicio + (horas * precioHora);
+    document.getElementById("total").textContent = total.toFixed(2) + "€";
+});
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        }
+    });
+}, { threshold: 0.2 });
+
+document.querySelectorAll(".blog-card").forEach(card => {
+    observer.observe(card);
+});
