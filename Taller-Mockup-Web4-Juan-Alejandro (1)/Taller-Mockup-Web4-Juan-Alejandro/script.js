2bfa09ee0abaa2bcc6ef283dfe641513b9337577
jmc0109-prog <jmc0109@alu.medac.es>
2026-04-16 18:01:35 +0200

    Update script.js

--- Taller-Mockup-Web4-Juan-Alejandro (1)/Taller-Mockup-Web4-Juan-Alejandro/script.js
+++ Taller-Mockup-Web4-Juan-Alejandro (1)/Taller-Mockup-Web4-Juan-Alejandro/script.js
@@ -5,19 +5,15 @@ botonesNav.forEach(boton => {
     boton.addEventListener('click', () => {
         const destino = boton.dataset.target;
 
-     
         botonesNav.forEach(b => b.classList.remove('active'));
         secciones.forEach(s => s.classList.remove('activa'));
 
-       
         boton.classList.add('active');
         document.getElementById(destino).classList.add('activa');
-
 
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 });
-
 document.getElementById("calcularBtn").addEventListener("click", () => {
     const servicio = parseFloat(document.getElementById("servicio").value);
     const horas = parseFloat(document.getElementById("horas").value);
@@ -32,7 +28,7 @@ document.getElementById("calcularBtn").addEventListener("click", () => {
     document.getElementById("total").textContent = total.toFixed(2) + "€";
 });
 
-
+// ANIMACIONES BLOG
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -45,39 +41,40 @@ document.querySelectorAll(".blog-card").forEach(card => {
     observer.observe(card);
 });
 
+// FIREBASE
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 
- import { initializeApp } from "firebase/app";
-  import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+import { getFirestore, collection, addDoc, getDocs } 
+from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyBHF9b1yQvd0qECUYgJPIaxWv0iN3qemjc",
     authDomain: "taller-mockup-web4.firebaseapp.com",
-    projectId:"taller-mockup-web4",
+    projectId: "taller-mockup-web4",
     storageBucket: "taller-mockup-web4.firebasestorage.app",
     messagingSenderId: "782151136268",
     appId: "1:782151136268:web:fc0f9e2fccb8fcbc19aa29",
     measurementId: "G-HQDEGQVVH5"
-  };
+};
 
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-  async function guardarEjemplo() {
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function guardarEjemplo() {
     await addDoc(collection(db, "usuarios"), {
-      nombre: "Juan",
-      edad: 18
+        nombre: "Juan",
+        edad: 18
     });
-  async function guardarEjemplo() {   
-    await addDoc(collection(db, "usuarios"), {
-    nombre: "Angel",
-    edad: 24
-  });
     console.log("Dato guardado");
-  }
-  async function leerEjemplo() {
+}
+
+async function leerEjemplo() {
     const querySnapshot = await getDocs(collection(db, "usuarios"));
     querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
+        console.log(doc.id, " => ", doc.data());
     });
-  }
-  guardarEjemplo();
-  leerEjemplo();
+}
+
+guardarEjemplo();
+leerEjemplo();
